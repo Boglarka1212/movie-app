00a5fd0a2a85c1835fb5ed1b66afb1b449e6768e
boglarka.kazmer <kazmer.boglarka@gmail.com>
2026-04-08 13:54:29 +0200

    commit

---
+++ oevelse.js
@@ -0,0 +1,82 @@
+"use strict";
+
+/* console.log("Hej fra Javascript!");
+
+const message = "javascript er sejt";
+console.log(message);
+
+const favoritMovie = "Inception";
+const year = 2010;
+const rating = 8.8;
+
+console.log("Min favorit film er:", favoritMovie);
+console.log("Den kom i:", year);
+console.log("Rating:", rating);
+
+const firstName = "Boglarka";
+let age = 24;
+const isStudent = true;
+const favoriteMovie = "The Vampire Diaries";
+let hobby = "Bagning";
+
+console.log("Navn:", firstName);
+console.log("Før:", age, hobby);
+console.log("Er studerende:", isStudent);
+console.log("Yndlingsfilm:", favoriteMovie);
+
+age = 25;
+hobby = "Træning";
+
+console.log("Efter:", age, hobby); */
+
+/* // Find h1 elementet
+const heading = document.querySelector("h1");
+console.log("Jeg fandt h1:", heading);
+
+// Find count span
+const countDisplay = document.querySelector("#counter");
+console.log("Jeg fandt count:", countDisplay);
+
+// Find knapperne
+const clickButton = document.querySelector("#click-button");
+const resetButton = document.querySelector("#reset-button");
+console.log("Jeg fandt knapperne:", clickButton, resetButton);
+
+// Ændre overskrift
+heading.textContent = "WOW, jeg kan ændre tekst";
+
+//Ændre count tallet
+countDisplay.textContent = "42";
+
+//Ændre tekstfarve
+heading.style.color = "yellow";
+
+//Ændre baggrund på count
+countDisplay.style.backgroundColor = "rgba(255, 255, 255, 0.2)";
+countDisplay.style.padding = "10px";
+countDisplay.style.borderRadius = "10px";
+
+
+
+
+"use strict";
+
+console.log("App starter....");
+
+// Find knappen
+const clickButton = document.querySelector("#click-button");
+
+//find h1
+const heading = document.querySelector("h1");
+
+//ændre h1
+heading.textContent = "Klikkeren";
+
+// LYt efter click
+clickButton.addEventListener("click", function () {
+  console.log("KLick");
+  heading.textContent = "Jubiiii, du gjorde det!";
+  heading.style.color = "pink";
+  heading.style.backgroundColor = "white";
+});
+ */
