e08131568aa6ae371d92ed6539f2529d328f309f
boglarka.kazmer <kazmer.boglarka@gmail.com>
2026-04-15 09:37:35 +0200

    første array oprettet, tjek i console

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
 "use strict";
 
 console.log("Movie App - DAG 2 starter...");
+
+const movies = [
+  "Inception",
+  "The Matrix",
+  "Interstellar",
+  "The Dark Knight",
+  "Pulp Fiction"
+];
+
+console.log("Alle film:", movies);
+console.log("første film:", movies[0]);
+console.log("anden film:", movies[1]);
+console.log("tredje film:", movies[2]);
+
+console.log("alle film:", movies.length);
+
+for (const movie of movies) {
+    console.log("Film:", movie);
+}
