acf12704b183c062fdc03a901c556bd415684fed
boglarka.kazmer <kazmer.boglarka@gmail.com>
2026-04-15 09:55:19 +0200

    movieList defineret og loop oprettet

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ const movies = [
   "The Matrix",
   "Interstellar",
   "The Dark Knight",
-  "Pulp Fiction"
+  "Pulp Fiction",
 ];
 
-console.log("Alle film:", movies);
+/* console.log("Alle film:", movies);
 console.log("første film:", movies[0]);
 console.log("anden film:", movies[1]);
 console.log("tredje film:", movies[2]);
@@ -19,4 +19,19 @@ console.log("alle film:", movies.length);
 
 for (const movie of movies) {
     console.log("Film:", movie);
+} */
+
+const movieList = document.querySelector("#movie-list");
+console.log(movieList);
+
+for (const movie of movies) {
+  const html = /* html */ `
+    <article class="movie-card">
+        <div class="movie-info">
+            <h3>${movie}</h3>
+        </div>
+    </article>
+    `;
+
+  movieList.insertAdjacentHTML("beforeend", html);
 }
