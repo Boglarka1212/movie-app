1970a6afdb302a9bd9f5405ebc79a9a012133f6f
boglarka.kazmer <kazmer.boglarka@gmail.com>
2026-04-15 10:07:37 +0200

    objekter og loop oprettet for 2 film - logget

--- app.js
+++ app.js
@@ -2,35 +2,36 @@
 
 console.log("Movie App - DAG 2 starter...");
 
+/* const movie = {
+  title: "Inception",
+  year: 2010,
+  rating: 8.8,
+}; */
+
+/* console.log("Titel:", movie.title);
+console.log("År:", movie.year);
+console.log("Rating:", movie.rating); */
+
 const movies = [
-  "Inception",
-  "The Matrix",
-  "Interstellar",
-  "The Dark Knight",
+  {
+    title: "Inception",
+    year: 2010,
+    rating: 8.8,
+  },
+  {
+    title: "The Matrix",
+    year: 1999,
+    rating: 8.7,
+  },
 ];
 
-/* console.log("Alle film:", movies);
-console.log("første film:", movies[0]);
-console.log("anden film:", movies[1]);
-console.log("tredje film:", movies[2]);
-
+console.log("Første film:", movies[0]);
+console.log("Anden film:", movies[1]);
 console.log("alle film:", movies.length);
 
-for (const movie of movies) {
-    console.log("Film:", movie);
-} */
-
-const movieList = document.querySelector("#movie-list");
-console.log(movieList);
 
 for (const movie of movies) {
-  const html = /* html */ `
-    <article class="movie-card">
-        <div class="movie-info">
-            <h3>${movie}</h3>
-        </div>
-    </article>
-    `;
-
-  movieList.insertAdjacentHTML("beforeend", html);
+    console.log("titel:", movie.title);
+    console.log("rating:", movie.rating);
+    
 }
