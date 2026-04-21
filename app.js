07dbcd2d71311619f6cc7650a429af81c52fb2ef
boglarka.kazmer <kazmer.boglarka@gmail.com>
2026-04-21 15:44:28 +0200

    opgave 3 færdig

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const MOVIES_URL =
   "https://raw.githubusercontent.com/cederdorff/race/refs/heads/master/data/movies.json";
 
 let allMovies = [];
-
 const movieList = document.querySelector("#movie-list");
+const genreSelect = document.querySelector("#genre-select");
 
 fetchMovies();
 
@@ -13,8 +13,49 @@ async function fetchMovies() {
   const response = await fetch(MOVIES_URL);
   allMovies = await response.json();
 
+  populateGenreSelect();
   showMovies(allMovies);
+
+  genreSelect.addEventListener("change", applyGenreFilter);
 }
+
+
+function populateGenreSelect() {
+  const genres = new Set();
+
+  for (const movie of allMovies) {
+    for (const genre of movie.genre) {
+      genres.add(genre);
+    }
+  }
+
+
+  const sortedGenres = [...genres].sort((a, b) => a.localeCompare(b));
+
+  for (const genre of sortedGenres) {
+    genreSelect.insertAdjacentHTML (
+      "beforeend",
+      `<option value="${genre}">${genre}</option>`,
+    );
+  }
+}
+
+function applyGenreFilter() {
+  const selectedGenre = genreSelect.value;
+
+  if (selectedGenre === "all") {
+    showMovies(allMovies);
+    return;
+  }
+
+  const filteredMovies = allMovies.filter(function (movie) {
+    return movie.genre.includes(selectedGenre);
+  });
+
+  showMovies(filteredMovies);
+}
+
+
 
 function showMovies(movies) {
   movieList.innerHTML = "";
