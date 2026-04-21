4c6810db2ee15afafafd8554018cdbf245a2b0e7
boglarka.kazmer <kazmer.boglarka@gmail.com>
2026-04-21 13:45:22 +0200

    opgave 1 løst

--- app.js
+++ app.js
@@ -1,49 +1,26 @@
 "use strict";
 
-const movies = [
-  {
-    title: "Inception",
-    year: 2010,
-    rating: 8.8,
-    image:
-      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_.jpg",
-  },
-  {
-    title: "The Matrix",
-    year: 1999,
-    rating: 8.7,
-    image: "https://m.media-amazon.com/images/I/51EG732BV3L.jpg",
-  },
-  {
-    title: "The Dark Knight",
-    year: 2008,
-    rating: 9.0,
-    image:
-      "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_.jpg",
-  },
-  {
-    title: "The Shawshank Redemption",
-    year: 1994,
-    rating: 9.3,
-    image: "https://m.media-amazon.com/images/I/51NiGlapXlL._AC_.jpg",
-  },
-  {
-    title: "Oppenheimer",
-    year: 2023,
-    rating: 8.4,
-    image:
-      "https://m.media-amazon.com/images/M/MV5BN2JkMDc5MGQtZjg3YS00NmFiLWIyZmQtZTJmNTM5MjVmYTQ4XkEyXkFqcGc@._V1_.jpg",
-  },
-  {
-    title: "Dune",
-    year: 2021,
-    rating: 8.0,
-    image:
-      "https://upload.wikimedia.org/wikipedia/en/8/8e/Dune_%282021_film%29.jpg",
-  },
-];
+const MOVIES_URL =
+  "https://raw.githubusercontent.com/cederdorff/race/refs/heads/master/data/movies.json";
+
+let allMovies = [];
 
 const movieList = document.querySelector("#movie-list");
+
+fetchMovies();
+
+async function fetchMovies() {
+  console.log("Henter film data...");
+
+  const response = await fetch(MOVIES_URL);
+  allMovies = await response.json();
+
+  console.log("Hentet", allMovies.length, "film!");
+  console.log("Første film:", allMovies[0]);
+  console.log("Alle film:", allMovies);
+
+  showMovies(allMovies);
+}
 
 showMovies(movies);
 
@@ -68,7 +45,6 @@ function showMovie(movie) {
 
   movieList.insertAdjacentHTML("beforeend", html);
 }
-
 
 function formatMovieTitle(title, year) {
   return `${title} (${year})`;
